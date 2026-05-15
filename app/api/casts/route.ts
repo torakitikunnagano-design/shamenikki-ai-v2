413ec983e2fcef8e35631b46ec4ae0db5ad10e09
torakitikunnagano-design <torakitikunnagano@gmail.com>
2026-05-15 10:20:11 +0900

    Update route.ts

--- app/api/casts/route.ts
+++ app/api/casts/route.ts
@@ -8,14 +8,33 @@ const supabase = createClient(
 export async function GET() {
   const { data, error } = await supabase
     .from("casts")
-    .select("*")
-    .order("created_at", {
-      ascending: false,
-    });
+    .select("*");
 
   if (error) {
     return Response.json([]);
   }
 
-  return Response.json(data);
+  return Response.json(data || []);
+}
+
+export async function POST(request: Request) {
+  const { name, is_active } = await request.json();
+
+  const { error } = await supabase
+    .from("casts")
+    .update({
+      is_active,
+    })
+    .eq("name", name);
+
+  if (error) {
+    return Response.json({
+      success: false,
+      error: error.message,
+    });
+  }
+
+  return Response.json({
+    success: true,
+  });
 }
